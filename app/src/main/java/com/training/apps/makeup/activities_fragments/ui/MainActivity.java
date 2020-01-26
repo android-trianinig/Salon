1d439704b2b07ea76e9dd747c7768f645dae4c3f
Ahmed Ramadan <AhmedShouman53@gmail.com>
2020-01-26 14:32:50 +0200

    ch

---
+++ app/src/main/java/com/training/apps/makeup/activities_fragments/ui/MainActivity.java
@@ -0,0 +1,16 @@
+package com.training.apps.makeup.activities_fragments.ui;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.training.apps.makeup.R;
+
+public class MainActivity extends AppCompatActivity  {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
