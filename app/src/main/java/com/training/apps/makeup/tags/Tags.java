1d99d752e89f2223ff78ff9999753a5de27c7a49
Ahmed Ramadan <AhmedShouman53@gmail.com>
2020-01-26 14:11:37 +0200

    ch

--- app/src/main/java/com/training/apps/makeup/tags/Tags.java
+++ app/src/main/java/com/training/apps/makeup/tags/Tags.java
@@ -1,12 +1,8 @@
 package com.training.apps.makeup.tags;
 
 public class Tags {
-    public static String base_url = "https://aamalnaa.com/";
-    public static final String IMAGE_Ads_URL= base_url+"src/images/";
-    public static final String IMAGE_user_URL= base_url+"src/avatars/";
-    public static final String IMAGE_banks_URL= base_url+"src/banks/";
+    public static String base_url = "";
 
-    public static final String IMAGE_message_URL= base_url+"src/messages/";
 
     public static final String session_login = "login";
     public static final String session_logout = "logout";
