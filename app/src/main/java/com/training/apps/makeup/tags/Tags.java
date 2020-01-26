e9bcb731d5d6e98ab2deb3ead6f36d5313c31fff
ARAB <ahmedmohamed23345@gmail.com>
2020-01-26 14:13:06 +0200

    ch

--- app/src/main/java/com/training/apps/makeup/tags/Tags.java
+++ app/src/main/java/com/training/apps/makeup/tags/Tags.java
@@ -1,7 +1,7 @@
 package com.training.apps.makeup.tags;
 
 public class Tags {
-    public static String base_url = "";
+    public static String base_url = "hhhhhh";
 
 
     public static final String session_login = "login";
